c8824b4eb6e04e389c58cb430e8b80b38c08136c
afshinfard <a.afshinfard@gmail.com>
2021-05-26 18:30:57 -0700

    progress

--- r/visualization/k_kc_sweep_results.R
+++ r/visualization/k_kc_sweep_results.R
@@ -1,4 +1,12 @@
 # Amirhossein Afshinfard 
-# afshinfard @ github
+# afshinfard @ github / aafshinfard @ github
 # aafshinfard@gmail.com
 
+#####
+# Input format: 3 files 1 per each kc value (kc 1:3), each file looks like:
+#| name_1 | NGA50_1 | NG50_1  | N50_1  | misassembly_1 | genome fraction_1 |   
+#| name_2 | NGA50_2 | NG50_2  | N50_2  | misassembly_2 | genome fraction_2 |   
+# ....
+# These file may be generated using:
+#   https://github.com/afshinfard/scripts/assembly/quast-analysis $$$$$$$$$$$$$$$$$$$$$
+#   https://github.com/afshinfard/scripts/assembly/quast-analysis $$$$$$$$$$$$$$$$$$$$$
