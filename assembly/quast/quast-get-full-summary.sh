efb21d45acf3a53fe8d0d938ca5eae441d0fa655
Amirhossein Afshinfard <31241012+afshinfard@users.noreply.github.com>
2021-05-26 18:39:15 -0700

    Create quast-get-full-summary.sh

---
+++ assembly/quast/quast-get-full-summary.sh
@@ -0,0 +1,21 @@
+# by Amirhossein Afshinfard 
+# afshinfard @ github / aafshinfard @ github
+# aafshinfard@gmail.com
+
+
+dir_pattern="2x*/*quast/"
+
+#### Summarize in Vlad's style
+# credits for quast-get-summary: @vnikolic
+echo "" > quast_full_summary.txt
+for d in ${dir_pattern} ; do     echo "$d">>quast_full_summary.txt; /projects/btl/aafshinfard/scripts/vnikolic/quast-get-summary "$d">>quast_full_summary.txt; done
+
+#### Print in a tablular format
+cat quast_full_summary.txt | awk '{if(NF<2) print "|"; else printf "\t"; printf "|%s", $(NF)} END {print "|"}'>quast_full_summary.tsv
+rm quast_full_summary.txt
+cat quast_full_summary.tsv
+
+#### ABySS specific: split based on kc value
+grep "kc2" quast_full_summary.tsv >quast_kc2_summary.tsv
+grep "kc3" quast_full_summary.tsv >quast_kc3_summary.tsv
+grep "kc4" quast_full_summary.tsv >quast_kc4_summary.tsv
