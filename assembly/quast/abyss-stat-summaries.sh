63497c3e2ba4ec4d5caccb97829a1e99d56bfb2d
Amirhossein Afshinfard <31241012+afshinfard@users.noreply.github.com>
2021-08-14 16:53:28 -0700

    Update abyss-stat-summaries.sh

--- assembly/quast/abyss-stat-summaries.sh
+++ assembly/quast/abyss-stat-summaries.sh
@@ -9,7 +9,7 @@ dir_pattern="assembly-k*/assembly-stats"
 echo "" > abyss_n50_summary.txt
 for d in ${dir_pattern} ; do     echo "$d">>abyss_n50_summary.txt; grep "scaffolds" ${d} | awk '{print $5"\t"$6}' >>abyss_n50_summary.txt; done
 cat abyss_n50_summary.txt | awk '{if(NF<2) print "|"; else printf "\t"; printf "| %s", $(NF)} END {print " |"}'>abyss_n50_summary.tsv
-
+rm abyss_n50_summary.txt
 sort -rnk4 abyss_n50_summary.tsv | head -n1
 
 
