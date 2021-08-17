f671ff2630a5fa41bb1627e84d0dfaa495c01ffb
Amirhossein Afshinfard <31241012+afshinfard@users.noreply.github.com>
2021-08-17 12:34:58 -0700

    Update quast-get-summaries.sh

--- assembly/quast/quast-get-summaries.sh
+++ assembly/quast/quast-get-summaries.sh
@@ -16,6 +16,7 @@ for d in ${dir_pattern} ; do     echo "$d">>quast_full_summary.txt; /projects/bt
 #### Print in a tablular format
 cat quast_full_summary.txt | awk '{if(NF<2) print "|"; else printf "\t"; printf "|%s", $(NF)} END {print "|"}'>quast_full_summary.tsv
 rm quast_full_summary.txt
+cat quast_full_summary.tsv
 
 #### ABySS specific: split based on kc value
 grep "kc2" quast_full_summary.tsv >quast_kc2_summary.tsv
