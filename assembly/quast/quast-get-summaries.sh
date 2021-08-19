97b7ced61f52faa9e81821d9ae015ceeb3cd0035
Amirhossein Afshinfard <31241012+afshinfard@users.noreply.github.com>
2021-08-18 17:03:18 -0700

    Update quast-get-summaries.sh

--- assembly/quast/quast-get-summaries.sh
+++ assembly/quast/quast-get-summaries.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 # by Amirhossein Afshinfard 
 # afshinfard @ github / aafshinfard @ github
 # aafshinfard@gmail.com
 
 # incomplete
 
-dir_pattern="2x*/*quast/"
+#dir_pattern="2x*/*quast/"
 dir_pattern="assembly-k*/*quast-*/"
 
 
@@ -19,6 +20,7 @@ rm quast_full_summary.txt
 cat quast_full_summary.tsv
 
 #### ABySS specific: split based on kc value
-grep "kc2" quast_full_summary.tsv >quast_kc2_summary.tsv
-grep "kc3" quast_full_summary.tsv >quast_kc3_summary.tsv
-grep "kc4" quast_full_summary.tsv >quast_kc4_summary.tsv
+#grep "kc2" quast_full_summary.tsv >quast_kc2_summary.tsv
+#grep "kc3" quast_full_summary.tsv >quast_kc3_summary.tsv
+#grep "kc4" quast_full_summary.tsv >quast_kc4_summary.tsv
+#grep "kc5" quast_full_summary.tsv >quast_kc4_summary.tsv
