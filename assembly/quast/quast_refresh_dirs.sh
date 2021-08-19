6ac9480a7e83830296283ddd3b66b71a7ada1ae3
afshinfard <a.afshinfard@gmail.com>
2021-08-18 18:19:32 -0700

    reg-updates

--- assembly/quast/quast_refresh_dirs.sh
+++ assembly/quast/quast_refresh_dirs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-dir_pattern="na*"
+dir_pattern="na24143*noextend*"
 
 #### Rerun "abyss-stat-summaries" and "quast-get-summaries" for all directories that match the pattern
 
-for d in ${dir_pattern} ; do  cd "$d"; /projects/btl/aafshinfard/github/scripts/assembly/quast/abyss-stat-summaries.sh; /projects/btl/aafshinfard/github/scripts/assembly/quast/quast-get-summaries.sh; done
+for d in ${dir_pattern} ; do  cd "$d"; /projects/btl/aafshinfard/github/scripts/assembly/quast/abyss-stat-summaries.sh; /projects/btl/aafshinfard/github/scripts/assembly/quast/quast-get-summaries.sh; cd ..; done
