12f325e301c95cb1f8b215cbb0b190a57041d040
afshinfard <a.afshinfard@gmail.com>
2021-08-18 17:01:17 -0700

    reg-updates

--- assembly/RVis/sweeps_general.R
+++ assembly/RVis/sweeps_general.R
@@ -20,17 +20,30 @@
 species="hsapiens"
 setwd("/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/figures/assembly_quality")
 
+
+exp1_n="na24143 k120_kc2 - N sweeps"
+exp1="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_N_sweeps_k120_kc2/quast_full_summary.tsv";
+
+
 exp1_n="na24143 +konnector N=20"
 exp1="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_konnector-noextend_sweeps/quast_full_summary.tsv";
+exp1_n="na24143 +konnector --extend N=20"
+exp1="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_konnector-extend_sweeps/quast_full_summary.tsv";
 
-exp2_n="na24143 +konnector N=30"
-exp2="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_konnector-noextend_sweeps_N30/quast_full_summary.tsv";
+exp2_n="na24143 +konnector N=10"
+exp2="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_konnector-noextend_sweeps_N10/quast_full_summary.tsv";
+exp2_n="na24143 +konnector --extend N=10"
+exp2="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_konnector-extend_sweeps_N10/quast_full_summary.tsv";
 
 exp3_n="na24143 - abyss 2.3.1 & N[10-20] "
 exp3="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers2/abyss2.5/na24143_2x250/quast_full_summary.tsv";
+exp3_n="na24143 - abyss 2.3.1 & N[10-20] "
+exp3="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers2/abyss2.5/na24143_2x250/quast_full_summary.tsv";
 
-exp4_n="na24385 (2x250)"
-exp4="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers2/abyss2.5/na24385_2x250/quast_full_summary.tsv";
+exp4_n="na24143 +konnector N=15"
+exp4="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_konnector-noextend_sweeps_N15/quast_full_summary.tsv";
+exp4_n="na24143 +konnector --extend N=15"
+exp4="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_konnector-extend_sweeps_N15/quast_full_summary.tsv";
 
 ####################################################
 
@@ -43,13 +56,40 @@ library(gridExtra)
 #
 reformat <- function(kcxf, exp_n){
   #kcxf = kcxf[1:length(kcxf)-1]
+  
+  
   colnames(kcxf)=c("kc","k","nga50","ng50","n50", "misassemblies", "fraction","exp_n")
   
   splits=strsplit(as.character(kcxf[,2]), split = "-", fixed = TRUE)
+  split=splits[[1]]
+  N_ind=NULL
+  S_ind=NULL
+  
+  for (i in 1:length(split)){
+    print(split[i])
+    if ( grepl("N", split[i], fixed = TRUE) ){
+      old_names=colnames(kcxf)
+      kcxf[,length(kcxf)+1] <- NA
+      colnames(kcxf)=c(old_names,"N")
+      N_ind=i
+    }
+    if ( grepl("S", split[i], fixed = TRUE) ){
+      old_names=colnames(kcxf)
+      kcxf[,length(kcxf)+1] <- NA
+      colnames(kcxf)=c(old_names,"S")
+      S_ind=i
+    }
+  }
   
   df <- data.frame(matrix(unlist(splits), nrow=length(splits), byrow=TRUE))
   kcxf[,2]=df[,2]
   kcxf[,1]=df[,3]
+  if (! is.null(N_ind)){
+    kcxf[,"N"]=df[,N_ind]
+  }
+  if (! is.null(S_ind)){
+    kcxf[,"S"]=df[,S_ind]
+  }
   
   splits=strsplit(as.character(df[,2]), split = "k")
   df2 <- data.frame(matrix(unlist(splits), nrow=length(splits), byrow=TRUE))
@@ -58,7 +98,20 @@ reformat <- function(kcxf, exp_n){
   splits=strsplit(as.character(df[,3]), split = "kc")
   df2 <- data.frame(matrix(unlist(splits), nrow=length(splits), byrow=TRUE))
   kcxf[,1]=as.numeric(as.character(df2[,2]))
-  kcxf[,length(kcxf)] = exp_n
+  
+  if (! is.null(N_ind)){
+    splits=strsplit(as.character(df[,N_ind]), split = "N")
+    df2 <- data.frame(matrix(unlist(splits), nrow=length(splits), byrow=TRUE))
+    kcxf[,"N"]=as.numeric(as.character(df2[,2]))
+  }
+  if (! is.null(S_ind)){
+    splits=strsplit(as.character(df[,S_ind]), split = "S")
+    df2 <- data.frame(matrix(unlist(splits), nrow=length(splits), byrow=TRUE))
+    kcxf[,"S"]=as.numeric(as.character(df2[,2]))
+  }
+  
+  kcxf[,"exp_n"] = exp_n
+  
   return (kcxf)
 }
 
@@ -74,8 +127,8 @@ plotter_k_vs_any <- function(experiment, data, y_par="ng50", xlimits=NULL, ylimi
                                      fill = "kc")) + #Algorithm)) 
     scale_color_manual(#name = "Algorithm", #"Tool", 
       #values = c("deepskyblue1","seagreen2","goldenrod1")) + #indianred2
-      values = c("mediumorchid3","turquoise3","goldenrod1")) +
-    scale_fill_manual(values = c("mediumorchid3","turquoise3","goldenrod1")) +
+      values = c("mediumorchid3","turquoise3","goldenrod1","orangered")) +
+    scale_fill_manual(values = c("mediumorchid3","turquoise3","goldenrod1","orangered")) +
     scale_shape_manual(values = c(1, 19, 21, 19, 19))+
     theme_minimal() +
     theme(
@@ -121,6 +174,65 @@ plotter_k_vs_any <- function(experiment, data, y_par="ng50", xlimits=NULL, ylimi
   return (frange50)
 }
 
+
+plotter_any_vs_all <- function(experiment, data, y_par="ng50", x_par="k", color_par="kc", xlimits=NULL, ylimits=NULL, breaks_count=0,
+                             x_breaks=NULL,
+                             x_breaks_labels=NULL,
+                             y_breaks=NULL,
+                             y_breaks_labels=NULL){
+  
+  range50 <- ggplot(data, aes_string(x_par,y_par , 
+                                     color = color_par, 
+                                     shape = color_par,#Sample,
+                                     fill = color_par)) + #Algorithm)) 
+    scale_color_manual(#name = "Algorithm", #"Tool", 
+      #values = c("deepskyblue1","seagreen2","goldenrod1")) + #indianred2
+      values = c("mediumorchid3","turquoise3","goldenrod1","orangered")) +
+    scale_fill_manual(values = c("mediumorchid3","turquoise3","goldenrod1","orangered")) +
+    scale_shape_manual(values = c(1, 19, 21, 19, 19))+
+    theme_minimal() +
+    theme(
+      axis.title = element_text(size = 15),
+      axis.text = element_text(size = 15, colour = "black"),
+      legend.title = element_text(size=15),
+      legend.text = element_text(size=15)
+    ) +
+    xlab(paste(x_par," value", sep=" ")) + ylab(as.character(y_par))
+  
+  #scale_y_continuous(limits = ylimits,
+  #                   #breaks=c(0,5e6,10e6,15e6,20e6,25e6,30e6,40e6,50e6,60e6,70e6,80e6 ),
+  #                   breaks=y_breaks,
+  #                   #labels=c("0","5 Mb","10 Mb","15 Mb","20 Mb","25 Mb","30 Mb","40 Mb",
+  #                   #        "50 Mb","60 Mb","70 Mb","80 Mb") )
+  #                   labels=y_breaks_labels )
+  #labels=c("0","5","10","15","20","25","30","40", "50","60","70","80") )
+  #return (range50)
+  if((! is.null(ylimits)) && (! is.null(y_breaks)) && (! is.null(y_breaks_labels)) ){
+    range50=range50+scale_y_continuous(limits = ylimits, breaks=y_breaks, labels = y_breaks_labels)
+  } else if (! is.null(ylimits)){
+    range50=range50+scale_y_continuous(limits = ylimits)
+  }
+  if((! is.null(xlimits)) && (! is.null(x_breaks)) ){
+    range50=range50+scale_x_continuous(limits = xlimits, breaks=x_breaks)
+  } else if (! is.null(xlimits)) {
+    range50=range50+scale_x_continuous(limits = xlimits)
+  } else if (! is.null(x_breaks)) {
+    range50=range50+scale_x_continuous(breaks=x_breaks)
+  }
+  
+  frange50 <- range50 + geom_line(aes_string(x=x_par, y= y_par), size=2, position=position_dodge(width=0.5)) + ggtitle(c(experiment,"k/kc sweep")) +
+    theme(
+      panel.grid.major.y = element_line(colour="gray", size=0.6, linetype=1,
+                                        lineend="round"),  # "round", "butt" or "square"
+      panel.grid.major.x = element_line( size=0.5, linetype=1,
+                                         lineend="round"),
+      panel.grid.minor.y = element_line( size=0.5 ),
+      panel.grid.minor.x = element_blank(),
+    )
+  frange50
+  
+  return (frange50)
+}
 ##
 
 exp1_f = read.table(exp1, header = FALSE, sep = "|", skip = 2)
@@ -128,6 +240,8 @@ exp2_f = read.table(exp2, header = FALSE, sep = "|", skip = 2)
 exp3_f = read.table(exp3, header = FALSE, sep = "|", skip = 2)
 exp4_f = read.table(exp4, header = FALSE, sep = "|", skip = 2)
 
+kcxf=exp1_f
+exp_n=exp1_n
 df_exp1 = reformat(exp1_f, exp1_n)
 df_exp2 = reformat(exp2_f, exp2_n)
 df_exp3 = reformat(exp3_f, exp3_n)
@@ -139,7 +253,7 @@ dim(df_exp3)
 dim(df_exp4)
 
 # df_kc2 = reformat(kc2f, 2)
-
+df=df_exp1
 df = rbind(df_exp1,df_exp2,df_exp3,df_exp4)
 #df = rbind(df_kc2, df_kc3, df_kc4)
 df[,"kc"] = as.factor(df[,"kc"])
@@ -151,15 +265,19 @@ colnames(df)
 ## facet_warp all together
 ## alternatively go to the next section for seperate plots with various k labels
 n50=plotter_k_vs_any(experiment, df, "n50")
+n50=plotter_any_vs_all(experiment, df, "n50", x_par="N")
 n50 <- n50 + facet_wrap(~exp_n, scales = "free_x", ncol = 4)
 
 ng50=plotter_k_vs_any(experiment, df, "ng50")
+ng50=plotter_any_vs_all(experiment, df, "ng50", x_par="N")
 ng50 <- ng50 + facet_wrap(~exp_n, scales = "free_x", ncol = 4)
 
 nga50=plotter_k_vs_any(experiment, df, "nga50")
+nga50=plotter_any_vs_all(experiment, df, "nga50", x_par="N")
 nga50 <- nga50 + facet_wrap(~exp_n, scales = "free_x", ncol = 4)
 
 miss=plotter_k_vs_any(experiment, df, "misassemblies", ylimits=c(0,2000))
+miss=plotter_any_vs_all(experiment, df, "misassemblies", x_par="N",ylimits=c(0,2000))
 miss <- miss + facet_wrap(~exp_n, scales = "free_x", ncol = 4)
 
 n50
@@ -169,6 +287,7 @@ miss
 
 library(gridExtra)
 
+grid.arrange(nga50, miss, ncol=1)
 grid.arrange(n50, ng50, nga50, miss, ncol=1)
 
 library(ggpubr)
@@ -192,6 +311,10 @@ ggarrange(n50+ rremove("xlab")+rremove("x.text")+ggtitle("")+theme(strip.text = 
 ylimit=c(0,125200)
 ybreaks=seq(0, 125000, by = 25000)
 ybreakslabs=c(c("0","25 Kb","50 Kb","75 Kb","100 Kb","125 Kb"))
+
+ylimit=c(0,175200)
+ybreaks=seq(0, 175000, by = 25000)
+ybreakslabs=c(c("0","25 Kb","50 Kb","75 Kb","100 Kb","125 Kb","150 Kb","175 Kb"))
 
 ylimit2=c(0,5200)
 ybreaks2=seq(0, 3000, by = 1000)
