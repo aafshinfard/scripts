4cb3f26d4e53de0243936646967e6c5dd5d5a368
Amirhossein Afshinfard <31241012+afshinfard@users.noreply.github.com>
2021-08-18 18:18:25 -0700

    Update quast_refresh_dirs.sh

--- assembly/quast/quast_refresh_dirs.sh
+++ assembly/quast/quast_refresh_dirs.sh
@@ -3,4 +3,4 @@ dir_pattern="na*"
 
 #### Rerun "abyss-stat-summaries" and "quast-get-summaries" for all directories that match the pattern
 
-for d in ${dir_pattern} ; do  cd "$d"; /projects/btl/aafshinfard/github/scripts/assembly/quast/abyss-stat-summaries.sh; /projects/btl/aafshinfard/github/scripts/assembly/quast/quast-get-summaries.sh; done
+for d in ${dir_pattern} ; echo "$d"; do  cd "$d"; /projects/btl/aafshinfard/github/scripts/assembly/quast/abyss-stat-summaries.sh; /projects/btl/aafshinfard/github/scripts/assembly/quast/quast-get-summaries.sh; done
