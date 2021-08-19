743aba36d598e68d501bc147dd7a163c5b0473eb
afshinfard <a.afshinfard@gmail.com>
2021-08-19 13:02:13 -0700

    reg-updates

--- assembly/RVis/sweeps_general.R
+++ assembly/RVis/sweeps_general.R
@@ -20,11 +20,15 @@
 species="hsapiens"
 setwd("/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/figures/assembly_quality")
 
-
-exp1_n="na24143 k120_kc2 - N sweeps"
-exp1="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_N_sweeps_k120_kc2/quast_full_summary.tsv";
-
-
+###________
+exp1_n="na12878 k90_kc2 - N sweeps"
+exp1="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na12878_N_sweeps_k90_kc2/quast_full_summary.tsv";
+exp2_n="na24143 k120_kc2 - N sweeps"
+exp2="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_N_sweeps_k120_kc2/quast_full_summary.tsv";
+###________
+exp1_n="na12878 k90_kc2_N15 - S sweeps"
+exp1="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na12878_k90_kc2_N15_S-sweeps/quast_full_summary.tsv";
+###________
 exp1_n="na24143 +konnector N=20"
 exp1="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_konnector-noextend_sweeps/quast_full_summary.tsv";
 exp1_n="na24143 +konnector --extend N=20"
@@ -45,6 +49,40 @@ exp4="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/n
 exp4_n="na24143 +konnector --extend N=15"
 exp4="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_konnector-extend_sweeps_N15/quast_full_summary.tsv";
 
+###________
+exp1_n="na12878 +konnector N=20"
+exp1="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na12878_konnector-noextend_sweeps/quast_full_summary.tsv";
+#exp1_n="na12878 +konnector --extend N=20"
+#exp1="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_konnector-extend_sweeps/quast_full_summary.tsv";
+
+exp2_n="na12878 +konnector N=15"
+exp2="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na12878_konnector-noextend_sweeps_N15/quast_full_summary.tsv";
+#exp2_n="na12878 +konnector --extend N=15"
+#exp2="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_konnector-extend_sweeps_N15/quast_full_summary.tsv";
+
+exp3_n="na12878 - abyss 2.3.1 & N[10-20] "
+exp3="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers2/abyss2.5/na12878_2x151/quast_full_summary.tsv";
+#exp3_n="na12878 - abyss 2.3.1 & N[10-20] "
+#exp3="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers2/abyss2.5/na24143_2x250/quast_full_summary.tsv";
+
+#exp4_n="na12878 +konnector N=15"
+#exp4="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_konnector-noextend_sweeps_N15/quast_full_summary.tsv";
+#exp4_n="na12878 +konnector --extend N=15"
+#exp4="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_konnector-extend_sweeps_N15/quast_full_summary.tsv";
+###________
+
+exp1_n="na24143 +konnector N10 - S sweeps"
+exp1="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_konnector-noextend_k150-kc4_N10_S-sweeps/quast_full_summary.tsv";
+
+exp2_n="na24143 +konnector N15 - S sweeps"
+exp2="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_konnector-noextend_k160-kc4-5_N=15_S-sweeps/quast_full_summary.tsv";
+
+exp3_n="na24143 +konnector N20 - S sweeps"
+exp3="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_konnector-noextend_k150-kc4_N20_S-sweeps/quast_full_summary.tsv";
+
+exp4_n="na24143 +konnector -- extend N15 - S sweeps"
+exp4="/projects/btl_scratch/aafshinfard/projects/abyss2.5/hsapiens/experiments/numbers3/t3_abyss2.5/na24143_konnector-extend_k160-kc3_N15-S-sweeps/quast_full_summary.tsv";
+
 ####################################################
 
 library(ggplot2)
@@ -53,6 +91,8 @@ library(gtable)
 library(grid)
 library(gridExtra)
 
+
+t2500=c(0,2500)
 #
 reformat <- function(kcxf, exp_n){
   #kcxf = kcxf[1:length(kcxf)-1]
@@ -60,7 +100,7 @@ reformat <- function(kcxf, exp_n){
   
   colnames(kcxf)=c("kc","k","nga50","ng50","n50", "misassemblies", "fraction","exp_n")
   
-  splits=strsplit(as.character(kcxf[,2]), split = "-", fixed = TRUE)
+  splits=strsplit(as.character(kcxf[,2]), split="-", fixed = TRUE)
   split=splits[[1]]
   N_ind=NULL
   S_ind=NULL
@@ -88,7 +128,12 @@ reformat <- function(kcxf, exp_n){
     kcxf[,"N"]=df[,N_ind]
   }
   if (! is.null(S_ind)){
-    kcxf[,"S"]=df[,S_ind]
+    S_value=df[,S_ind]
+    if (grepl("/", S_value, fixed = TRUE)){
+      S_value=strsplit(as.character(S_value), split="/", fixed = TRUE)
+    }
+    for (i in 1:length(S_value))
+      kcxf[i,"S"]=S_value[[i]][1]
   }
   
   splits=strsplit(as.character(df[,2]), split = "k")
@@ -105,7 +150,7 @@ reformat <- function(kcxf, exp_n){
     kcxf[,"N"]=as.numeric(as.character(df2[,2]))
   }
   if (! is.null(S_ind)){
-    splits=strsplit(as.character(df[,S_ind]), split = "S")
+    splits=strsplit(as.character(kcxf[,"S"]), split = "S")
     df2 <- data.frame(matrix(unlist(splits), nrow=length(splits), byrow=TRUE))
     kcxf[,"S"]=as.numeric(as.character(df2[,2]))
   }
@@ -229,7 +274,7 @@ plotter_any_vs_all <- function(experiment, data, y_par="ng50", x_par="k", color_
       panel.grid.minor.y = element_line( size=0.5 ),
       panel.grid.minor.x = element_blank(),
     )
-  frange50
+  frange50=frange50+theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
   
   return (frange50)
 }
@@ -242,6 +287,8 @@ exp4_f = read.table(exp4, header = FALSE, sep = "|", skip = 2)
 
 kcxf=exp1_f
 exp_n=exp1_n
+kcxf=exp2_f
+exp_n=exp2_n
 df_exp1 = reformat(exp1_f, exp1_n)
 df_exp2 = reformat(exp2_f, exp2_n)
 df_exp3 = reformat(exp3_f, exp3_n)
@@ -254,7 +301,11 @@ dim(df_exp4)
 
 # df_kc2 = reformat(kc2f, 2)
 df=df_exp1
+df = rbind(df_exp1,df_exp2)
+df = rbind(df_exp1,df_exp2,df_exp3)
+df = rbind(df_exp2,df_exp3,df_exp4)
 df = rbind(df_exp1,df_exp2,df_exp3,df_exp4)
+
 #df = rbind(df_kc2, df_kc3, df_kc4)
 df[,"kc"] = as.factor(df[,"kc"])
 df[,"exp_n"] = as.factor(df[,"exp_n"])
@@ -265,20 +316,28 @@ colnames(df)
 ## facet_warp all together
 ## alternatively go to the next section for seperate plots with various k labels
 n50=plotter_k_vs_any(experiment, df, "n50")
-n50=plotter_any_vs_all(experiment, df, "n50", x_par="N")
-n50 <- n50 + facet_wrap(~exp_n, scales = "free_x", ncol = 4)
+n50=plotter_any_vs_all("experiment", df, "n50", x_par="k",x_breaks = df_exp1[,"k"], x_breaks_labels = df_exp1[,"k"])
+n50=plotter_any_vs_all("experiment", df, "n50", x_par="N",x_breaks = df[,"N"], x_breaks_labels = df[,"N"])
+n50=plotter_any_vs_all("experiment", df, "n50", x_par="S",x_breaks = df[,"S"], x_breaks_labels = df[,"S"], xlimits = t2500)
+n50 <- n50 + facet_wrap(~exp_n, scales = "free_x", ncol = 4)+theme(strip.text = element_text(size=15))
 
 ng50=plotter_k_vs_any(experiment, df, "ng50")
-ng50=plotter_any_vs_all(experiment, df, "ng50", x_par="N")
-ng50 <- ng50 + facet_wrap(~exp_n, scales = "free_x", ncol = 4)
+ng50=plotter_any_vs_all("experiment", df, "ng50", x_par="k",x_breaks = df_exp1[,"k"], x_breaks_labels = df_exp1[,"k"])
+ng50=plotter_any_vs_all("experiment", df, "ng50", x_par="N")
+ng50=plotter_any_vs_all("experiment", df, "ng50", x_par="S",x_breaks = df[,"S"], x_breaks_labels = df[,"S"])
+ng50 <- ng50 + facet_wrap(~exp_n, scales = "free_x", ncol = 4)+theme(strip.text = element_text(size=15))
 
 nga50=plotter_k_vs_any(experiment, df, "nga50")
-nga50=plotter_any_vs_all(experiment, df, "nga50", x_par="N")
-nga50 <- nga50 + facet_wrap(~exp_n, scales = "free_x", ncol = 4)
+nga50=plotter_any_vs_all("experiment", df, "nga50", x_par="k",x_breaks = df_exp1[,"k"], x_breaks_labels = df_exp1[,"k"])
+nga50=plotter_any_vs_all("experiment", df, "nga50", x_par="N")
+nga50=plotter_any_vs_all("experiment", df, "nga50", x_par="S",x_breaks = df[,"S"], x_breaks_labels = df[,"S"], xlimits = t2500)
+nga50 <- nga50 + facet_wrap(~exp_n, scales = "free_x", ncol = 4)+theme(strip.text = element_text(size=15))
 
-miss=plotter_k_vs_any(experiment, df, "misassemblies", ylimits=c(0,2000))
-miss=plotter_any_vs_all(experiment, df, "misassemblies", x_par="N",ylimits=c(0,2000))
-miss <- miss + facet_wrap(~exp_n, scales = "free_x", ncol = 4)
+miss=plotter_k_vs_any("experiment", df, "misassemblies", ylimits=c(0,2000))
+miss=plotter_any_vs_all("experiment", df, "misassemblies", x_par="k",ylimits=c(0,2000), x_breaks = df_exp1[,"k"], x_breaks_labels = df_exp1[,"k"])
+miss=plotter_any_vs_all("experiment", df, "misassemblies", x_par="N",ylimits=c(0,2000))
+miss=plotter_any_vs_all("experiment", df, "misassemblies", x_par="S",ylimits=c(0,2000),x_breaks = df[,"S"], x_breaks_labels = df[,"S"], xlimits = t2500)
+miss <- miss + facet_wrap(~exp_n, scales = "free_x", ncol = 4)+theme(strip.text = element_text(size=15))
 
 n50
 ng50
--- assembly/quast/quast_refresh_dirs.sh
+++ assembly/quast/quast_refresh_dirs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-dir_pattern="na24143*noextend*"
+dir_pattern="na24*S*"
 
 #### Rerun "abyss-stat-summaries" and "quast-get-summaries" for all directories that match the pattern
 
-for d in ${dir_pattern} ; do  cd "$d"; /projects/btl/aafshinfard/github/scripts/assembly/quast/abyss-stat-summaries.sh; /projects/btl/aafshinfard/github/scripts/assembly/quast/quast-get-summaries.sh; cd ..; done
+for d in ${dir_pattern} ; do  cd "$d"; echo "$d"; /projects/btl/aafshinfard/github/scripts/assembly/quast/abyss-stat-summaries.sh; /projects/btl/aafshinfard/github/scripts/assembly/quast/quast-get-summaries.sh; cd ..; done
 
