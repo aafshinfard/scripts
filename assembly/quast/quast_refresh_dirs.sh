eda60193a39ffa8cbc84e2fcd1ef78833d7b167c
Amirhossein Afshinfard <31241012+afshinfard@users.noreply.github.com>
2021-08-23 10:27:05 -0700

    Update quast_refresh_dirs.sh

--- assembly/quast/quast_refresh_dirs.sh
+++ assembly/quast/quast_refresh_dirs.sh
@@ -2,9 +2,16 @@
 # inputs:
 # 1 - dir_pattern: pattern of directories to run quast-get-summaries and abyss-stat-summaries in (they may contain multiple or single assembly directories themselves)
 
-dir_pattern=${1:"na24*S*"}
-
+dir_pattern=${1:-"na24*S*"}
+root_dir=$(pwd)
 #### Rerun "abyss-stat-summaries" and "quast-get-summaries" for all directories that match the pattern
 
-for d in ${dir_pattern} ; do  cd "$d"; echo "$d"; /projects/btl/aafshinfard/github/scripts/assembly/quast/abyss-stat-summaries.sh; /projects/btl/aafshinfard/github/scripts/assembly/quast/quast-get-summaries.sh; cd ..; done
+for d in ${dir_pattern} ; do
+  cd "$d"
+  echo "Running `quast-get-summaries` and `abyss-stat-summaries` at:"
+  echo "    $d"
+  /projects/btl/aafshinfard/github/scripts/assembly/quast/abyss-stat-summaries.sh
+  /projects/btl/aafshinfard/github/scripts/assembly/quast/quast-get-summaries.sh
+  cd ${root_dir}
+done
 
