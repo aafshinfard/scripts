049570ca2f1ec7a48c25dd3daf19123fd0992e34
Amirhossein Afshinfard <31241012+afshinfard@users.noreply.github.com>
2021-08-25 12:57:45 -0700

    Create quast-run-dirs.sh

---
+++ assembly/quast/quast-run-dirs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dir_pattern=${1:-"assembly-k*/*quast-*/"}
+t=${2:-48}
+
+root_dir=$(pwd)
+for d in ${dir_pattern} ; do
+  cd "$d"
+  echo "Running quast at:"
+  echo "    $d"
+  /projects/btl/aafshinfard/tools/quast/quast-5.0.2/quast.py --no-icarus ${file} -r $ref --large --threads $t --output-dir quast-detailed-${file} > quast-detailed-${file}.out 2>quast-detailed-${file}.err # 2>&1 | tee quast-detailed-${file}.log
+  cd ${root_dir}
+done
+
+
+
+
